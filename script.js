af3ff88b58b2d1fa065599223e708cd678f5413f
azranmor66 <mor@azran.cc>
2025-12-04 21:03:35 +0200

    Fix logo URL in brands array

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ videos: [
     
     // רשימת הלוגואים לשיתופי פעולה
     brands: [
-        "https://raw.githubusercontent.com/azranmor66/azran/main/assets/images/katamon.png",
+        "azran/assets/images/sav.jpg",
         "azran/assets/images/tinok.webp",
         "azran/assets/images/ibi.png" 
     ],
